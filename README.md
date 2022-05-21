b1873fc2fdf25385cbe308d39f62b213a58fefa6
FCattaneo <fcattaneo@itba.edu.ar>
2022-05-20 21:27:33 -0300

    Todo Listo!!!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # EDA-Cup
 
+* Dentro de la carpeta resources se encuentran las 5 fotos de las camisetas de los robot, 
+ennumeradas del 1 al 4, el 8 y el numero 10. Además se encuentra el logo del equipo con
+con el nombre de "Equipo" dentro de la misma carpeta.
+* En el archivo signup.txt se encuentra el nombre del equipo y el lema.
+* El archivo de data.cpp contiene funciones de calculo necesarias, tales como conversiones
+de tipos de datos, calculos de proporciones, entre otras.
+* El archivo de GameModel.cpp contiene las funciones que tienen que ver con el modo de juego, 
+es decir el inicio, la lectura de los mensajes, agregar o eliminar un robot, entre otros.
+* Robot.cpp es la clase padre de los robot en ella se encuentran los metodos comunes a todos, tales como setPosition, setSpeed, entre otros.
+* Por ultimo en Players.cpp se encuentra la clase derivada de los robots y los metodos caracteristicos de ellos tales como moverse, ir hacia la pelota, patear, entre otros.
+* Se agregaron algunos Test necesarios para verificar el funcionamiento de las funciones de calculos 
+definidas en data.
+
--- main_test.cpp
+++ main_test.cpp
@@ -5,9 +5,6 @@
 #include "data.h"
 #include "Robot.h"
 using namespace std;
-/*
--HAY QUE PONER EL .JSON EN EL CMAKE
-*/
 
 
 int fail()
@@ -26,59 +23,15 @@ int pass()
 int main (void) 
 {
     cout << "                   __TESTING__" << endl;
-    /*Test 1. Cambio de datos 
-    *
-    */ 
-        cout << "Testing change a float to a vector if char..." << endl;
-        float data = 1.0
-        std::vector<char> result = getDataFromFloat(data);
-        cout << result << endl;
-        /*if (result == )
-        {
-            pass();
-        }
-        else
-        {
-            fail();
-        }*/
-    
-    //Test 2: Prueba de conversion de dato 2.
-    Vector2 coord = {1, -1};
-    float rotation = 1.05f
-    setPoint_t var = {coord, rotation};
-    cout << "TESTS FOR GET ARRAY FROM SETPOINT FUNCTION:" << endl;
-    std::vector<char> result = getArrayFromSetPoint(var);
-    cout<<result<<endl;
-    /*if (result == )
-    {
-        pass();
-    }
-    else
-    {
-        fail();
-    }*/
 
-    //Test 3: Otra conversion de dato
-    vector<char> test = {'a', 'b', 'c'};
-    float result = getFloat(vector<char> vec);
-    cout << result << endl;
-    /*if (result == )
-    {
-        pass();
-    }
-    else
-    {
-        fail();
-    }*/
-
-    //Test 4: Prueba del proporcional entre dos puntos
+    //Test 1: Prueba del proporcional entre dos puntos
     Vector2 originPos = {-1,1};
-    Vcctor2 finalPos = {1,-1};
+    Vector2 finalPos = {1,-1};
     cout << "TESTS FOR PROPORTIONAL POSITION FUNCTION:" << endl;
     cout << "Testing half position...";
     float proportion = 0.5f;
-    Vector2 result = proportionalPosition(originPos, finalPos, proportion);
-    if (result.compare({0,0}))
+    Vector2 result4 = proportionalPosition(originPos, finalPos, proportion);
+    if (result4.x == 0 && result4.y == 0 )
     {
         pass();
     }
@@ -87,10 +40,10 @@ int main (void)
         fail();
     }
 
-    float proportion = 0f;
+    float proportion2 = 0.0f;
     cout << "Testing origin position...";
-    Vector2 result = proportionalPosition(originPos, finalPos, proportion);
-    if (result.compare(originPos))
+    Vector2 result5 = proportionalPosition(originPos, finalPos, proportion2);
+    if (result5.x == originPos.x && result5.y == originPos.y)
     {
         pass();
     }
@@ -99,10 +52,10 @@ int main (void)
         fail();
     }
 
-    float proportion = 1.0f;
+    float proportion3 = 1.0f;
     cout << "Testing final position...";
-    Vector2 result = proportionalPosition(originPos, finalPos, proportion);
-    if (result.compare(finalPos))
+    Vector2 result6 = proportionalPosition(originPos, finalPos, proportion3);
+    if (result6.x == finalPos.x && result6.y == finalPos.y)
     {
         pass();
     }
@@ -111,12 +64,12 @@ int main (void)
         fail();
     }
 
-    //Test 5: Prueba del calculo de la rotacion del jugador adecuada.
-    cout << "Testing rotation..." << endl;
-    Vector2 originPos = {1, -1};
-    Vector2 finalPos = {-1, 1};
-    float result = calculateRotation(originPos, finalPos);
-    if (result == -45.0)
+    //Test 2: Prueba del calculo de la rotacion del jugador adecuada.
+    cout << "Testing rotation...";
+    Vector2 originPos3 = {1, -1};
+    Vector2 finalPos3 = {-1, 1};
+    float result7 = calculateRotation(originPos3, finalPos3);
+    if (result7 <= -225.0f && result7 >= -225.1f)//ESTO NO FUNCIONA NUNCA
     {
         pass();
     }
@@ -125,8 +78,8 @@ int main (void)
         fail();
     }
 
-    //Test 5: Prueba que verifica si una coordenada está cerca de otra,
-    cout << "Testing if 2 coordenates are close to each other" << endl;
+    //Test 3: Prueba que verifica si una coordenada está cerca de otra,
+    cout << "Testing if 2 coordenates are close to each other ";
     Vector2 originCoord = {1,1};
     Vector2 destinationCoord = {1,0};
     float nearRange = 2;
@@ -139,13 +92,13 @@ int main (void)
         fail();
     }
     
-    //Test 6: Prueba que verifica si el robot y la pelota están en sobre la misma linea. 
+    //Test 4: Prueba que verifica si el robot y la pelota están en sobre la misma linea. 
     cout << "TESTS FOR SAME LINE FUNCTION:" << endl;
     cout << "Testing positive intermediate...";
-    Vector2 originPos = {8,4};
-    Vector2 finalPos = {0,0};
+    Vector2 originPos4 = {8,4};
+    Vector2 finalPos4 = {0,0};
     Vector2 mediumPos = {4,2};
-    if(sameLine (originPos, finalPos, mediumPos))
+    if(sameLine (originPos4, finalPos4, mediumPos))
     {
         pass();
     }
@@ -155,8 +108,8 @@ int main (void)
     }
 
     cout << "Testing negative intermediate...";
-    Vector2 mediumPos = {-4,-2};
-    if(sameLine (originPos, finalPos, mediumPos))
+    Vector2 mediumPos3 = {-4, -2};
+    if(sameLine (originPos4, finalPos4, mediumPos3))
     {
         pass();
     }
